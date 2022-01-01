79f64dd76e8c5045a6c22e52ebf79b64bab8aa79
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-01-01 23:40:23 +0100

    Fixed some minor issues and files disorder

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,66 @@
 // Contact form Submission
 const form = document.querySelector("#contact-form");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const data = {
-    name: document.querySelector("#name").value,
-    email: document.querySelector("#email").value,
-    message: document.querySelector("#message").value,
-  };
+if (form != undefined) {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const data = {
+      name: document.querySelector("#name").value,
+      email: document.querySelector("#email").value,
+      message: document.querySelector("#message").value,
+    };
 
-  Swal.fire(
-    "Message Sent Successfully!",
-    "Your message is as follows:<br />Name: " +
-      data.name +
-      "<br />Email: " +
-      data.email +
-      (data.message ? "<br />Message: " + data.message : ""),
-    "success"
-  ).then(() => {
-    form.submit();
+    Swal.fire(
+      "Message Sent Successfully!",
+      "Your message is as follows:<br />Name: " +
+        data.name +
+        "<br />Email: " +
+        data.email +
+        (data.message ? "<br />Message: " + data.message : ""),
+      "success"
+    ).then(() => {
+      form.submit();
+    });
   });
-});
+}
+
+// Gallery generation
+const gallery = document.querySelector("#gallery .cars");
+
+if (gallery != undefined) {
+  fetch("./assets/db/cars.json")
+    .then((res) => res.json())
+    .then((data) => populateGallery(data));
+}
+
+function populateGallery(cars) {
+  if (cars != null) {
+    if (gallery.id != "home-gallery-cars")
+      for (let car of cars) {
+        gallery.appendChild(createCar(car));
+      }
+    else
+      for (let i = 0; i < 3; i++) {
+        gallery.appendChild(createCar(cars[i]));
+      }
+  }
+}
+
+function createCar(car) {
+  const carImg = document.createElement("img");
+  carImg.src = car.image;
+  carImg.alt = car.caption;
+
+  const carCaption = document.createElement("figcaption");
+  carCaption.innerText = car.caption;
+
+  const carCard = document.createElement("figure");
+  carCard.appendChild(carImg);
+  carCard.appendChild(carCaption);
+
+  const carDiv = document.createElement("div");
+  carDiv.classList.add("car");
+  carDiv.appendChild(carCard);
+
+  return carDiv;
+}
