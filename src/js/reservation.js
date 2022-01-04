deb61c1cc2e3291d79686fa4bea83ab21b53eaf8
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-01-04 11:10:48 +0100

    completion de reservation

--- src/js/reservation.js
+++ src/js/reservation.js
@@ -3,47 +3,105 @@ const reservation = document.querySelector("#reservation-form");
 const vehicles = document.querySelector("#vehicles");
 const vehicleOptions = document.querySelectorAll("#vehicles option");
 const bav = document.getElementsByName("bav");
+const moteur = document.querySelector("#moteur");
+const moteurOptions = document.querySelectorAll("#moteur option");
+const jours = document.querySelector("#jours");
+const rebriquePrix = document.querySelector("#rebriquePrix");
+const moteurTTC = {
+  electrique: 0.05,
+  hybride: 0.09,
+  essence: 0.14,
+  diesel: 0.21,
+};
+let price = {
+  net: 0,
+  ttc: 0,
+};
+let vehicleData = null;
 
 // if checkbox is checked, uncheck the other
-bav.forEach((b) => {
-  b.addEventListener("click", (event) => {
-    if (event.target.checked) {
-      bav.forEach((b) => {
-        if (b.id != event.target.id) b.checked = false;
-      });
-    }
-  });
-});
+// bav.forEach((b) => {
+//   b.addEventListener("click", (event) => {
+//     if (event.target.checked) {
+//       bav.forEach((b) => {
+//         if (b.id != event.target.id) b.checked = false;
+//       });
+//     }
+//   });
+// });
 
-// disable unavailable options
+// Get data from file
 fetch("./assets/db/vehicles.json")
   .then((res) => res.json())
   .then((data) => renderVehicle(data));
 
 function renderVehicle(vehiclesData) {
   vehicles.addEventListener("change", (event) => {
-    bav.forEach((b) => (b.checked = false));
+    // Reset Form
     if (event.target.value == "--") {
       reservation.reset();
+      bav.forEach((b) => {
+        b.checked = false;
+      });
+      moteurOptions[0].selected = true;
+      jours.value = 1;
+      jours.disabled = true;
     } else {
       // Find selected vehicle data
-      console.log(event.target.value);
-      const vehicle = vehiclesData.filter(
-        (v) => v.type == event.target.value
-      )[0];
-      console.log(vehicle);
+      vehicleData = vehiclesData.filter((v) => v.type == event.target.value)[0];
 
-      // Disable unavailable Checkboxes
+      // Check available BAV
       bav.forEach((b) => {
-        if (!vehicle.bav.includes(b.value)) b.disabled = true;
-        else b.disabled = false;
+        if (vehicleData.bav == b.value) {
+          b.checked = true;
+        } else b.checked = false;
       });
 
-      // // Disable unavailable Moteurs
-      // vehicleOptions.forEach((o) => {
-      //   if (vehicle.options.includes(o.value)) o.disabled = false;
-      //   else o.disabled = true;
-      // });
+      // reset moteur options
+      moteurOptions[0].selected = true;
+
+      // Disable unavailable Moteurs
+      moteurOptions.forEach((o) => {
+        console.log(o.disabled);
+        if (!vehicleData.moteur.includes(o.value)) o.disabled = true;
+        else o.disabled = false;
+      });
+
+      // Enable jours
+      jours.disabled = false;
+
+      // Calculate price without moteur
+      price.net =
+        vehicleData.bav == "auto"
+          ? vehicleData.tarif + vehicleData.tarif * 0.19
+          : vehicleData.tarif;
+
+      // Append price to the form
+      document.querySelector("#prix").value = price.net;
     }
   });
 }
+
+moteur.addEventListener("change", (event) => {
+  if (!event.target.value == "--") {
+    // Reset price to net
+    price.ttc = price.net;
+
+    // Append price to the form
+    document.querySelector("#prix").value = price.net;
+  } else {
+    // Calculate price with moteur
+    price.ttc = price.net + price.net * moteurTTC[event.target.value];
+
+    // Append price to the form
+    document.querySelector("#prix").value = price.ttc;
+  }
+});
+
+jours.addEventListener("change", (event) => {
+  // Calculate price with number of days
+  price.ttc = price.ttc * event.target.value;
+
+  // Append price to the form
+  document.querySelector("#prix").value = price.ttc;
+});
